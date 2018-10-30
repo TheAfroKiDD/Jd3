6518653cb3fa9fe4cde5c19737af8f3e956252cc
Riaz Thompson <Riazthompson02@gmail.com>
2018-10-30 13:00:06 +0000

    Local Initial Commit

---
+++ jd3/catalogue/BetterBasket.java
@@ -0,0 +1,23 @@
+package catalogue;
+
+import java.io.Serializable;
+import java.util.Collections;
+
+/**
+ * Write a description of class BetterBasket here.
+ * 
+ * @author  Riaz Thompson
+ * @version 1.0
+ */
+public class BetterBasket extends Basket implements Serializable
+{
+  private static final long serialVersionUID = 1L;
+  
+  public void Sort() {}
+  
+  
+ public void Merge() {}
+ 
+
+  // You need to add code here
+}
